533410141aaf579e006a37f01542e5a95514fa59
arpan13 <arpankumarpal728@gmail.com>
2021-06-17 22:04:21 +0530

    Added team members in the admin panel and connected it to the front end

--- tubers/webpages/views.py
+++ tubers/webpages/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
-
+from .models import Slider,Team
 # Create your views here.
 
 def home(request):
-    return render(request,'webpages/home.html')
+    sliders = Slider.objects.all()
+    teams = Team.objects.all()
+    data = {
+        'sliders':sliders,
+        'teams':teams
+    }
+    return render(request,'webpages/home.html',data)
 
 def about(request):
      return render(request,'webpages/about.html')
